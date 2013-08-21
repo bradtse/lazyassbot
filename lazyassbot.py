2723e6af6e435bff095e30530f41b11c38cd2c35
Bradley Tse <bradleytse@gmail.com>
2013-08-21 01:45:23 -0400

    Cleaned up code a lot. Cleaned up and updated the logging. Add in more checks, including Jiffy cehck

--- lazyassbot.py
+++ lazyassbot.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 """
-Parses through the comments looking for any mention of a time, and then uses
-this time to post a reply to the parent comment containing a link for video
-but for the specific time that they mention.
+A simple bot that replies with the corresponding Youtube time link when someone 
+comments on a specific time in the original video.
 """
 
-import os
 import sys
 import logging
 import time
@@ -17,55 +15,64 @@ import HTMLParser
 import pickle
 from collections import deque
 from urllib2 import HTTPError, URLError
-from pprint import pprint as pprint
 
 import praw
 
-# Constants
-READ_RECORD = 'recently_read.pkl'
+# File that holds the 'handled' deque when the bot is stopped. Prevents 
+# re-handling of comments that have already been handled before the bot was stopped
+HANDLED_FILE = 'recently_read.pkl'
+
+# Bot info
 BOT_NAME = u'lazyassbot'
 BOT_FAQ = 'http://www.reddit.com/r/lazyassbot/wiki/faq'
 BOT_FEEDBACK = 'http://www.reddit.com/r/lazyassbot/submit'
+
+# The footer that is attached to all comment replies
 FOOTER = u'''\n
 | *Incorrect? Downvote me!* | *Currently in beta* | [*FAQ*]({0}) | 
 [*Report Bug/Feedback*]({1}) |'''.format(BOT_FAQ, BOT_FEEDBACK)
 
+JIFFY_REGEX = re.compile(r'^.*?Jiffy!.*$')
+
+# regex used to match against the comment's text
 TIME_REGEX = re.compile(r'^.*?(?:[\W ]|^)(\d{0,2}\:\d{2})(?:\W|ish|$)'
                          '(?!.?(?:am|pm|morning|afternoon)\W).*$',
                          re.IGNORECASE|re.MULTILINE)
-START_TIME = time.time()
 
-# Youtube constants
+# Grabs the unique Youtube ID from a Youtube URL
 YT_REGEX = re.compile('^(?:https?://)?(?:www\.)?(?:youtu\.be/|youtube\.com'
-            '(?:/embed/|/v/|/watch\?v=|/watch\?.+&v=))([\w-]{11})(?:.+)?$')
+                 '(?:/embed/|/v/|/watch\?v=|/watch\?.+&v=))([\w-]{11})(?:.+)?$')
+# The URL that is used to get data on a specific Youtube ID
 YT_GDATA = 'http://gdata.youtube.com/feeds/api/videos/{0}?alt=json'
 
-LINK_REGEX = re.compile('(?:https?://)?(?:www\.)?(?:youtu\.be/|youtube\.com)',
+# Matches any kind of link
+# This needs to be fixed to account for more dns names
+LINK_REGEX = re.compile('(?:https?://)?(?:www\.)?.*?\.com',
                         re.MULTILINE)
 
-MATCH_ALL = re.compile(r'^.*?(\d{0,2}\:\d{2}).*$', re.IGNORECASE|re.MULTILINE)
-
 # Configure some basic logging
-logging.basicConfig(filename='lazyassbot.log',
+logging.basicConfig(filename='logs/lazyassbot-{0}.log'.format(time.ctime().replace(' ', '-')),
                     format='%(asctime)s :: %(levelname)s :: %(module)s :: %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     filemode='w',
                     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-# Initialize the Reddit object we will be using
-USER_AGENT = ("A bot that posts the youtube time link when a user comments \
-               about a specific time in the video. Responds via \
-               /u/lazyassbot. Written by /u/btse")
+# Initialize the Reddit object we will be using throughout
+USER_AGENT = ("A bot that posts the youtube time link when a user comments "
+              "about a specific time in the video. Responds via " 
+              "/u/lazyassbot. Written by /u/btse")
 REDDIT = praw.Reddit(user_agent=USER_AGENT)
 
 
+
 def login(user=None, password=None, filename='login.txt'):
-    """ Logins in to Reddit using the user/password provided by login.txt """
+    """ Logs in to Reddit using the user/password provided by login.txt """
     if user is None and password is None:
         with open(filename, 'rb') as f:
             user = f.readline().strip()
             password = f.readline().strip()
+    # TODO There needs to be more proper checks here
     REDDIT.login(user, password)
 
 def get_comments(subreddit_='videos', limit_=100):
@@ -75,127 +82,176 @@ def get_comments(subreddit_='videos', limit_=100):
 def get_youtube_id(url):
     """
     Returns the youtube id for a given url or None if it is not a youtube link
-    or improperly formatted
+    or is improperly formatted. 
     """
     return YT_REGEX.match(url).group(1) if YT_REGEX.match(url) else None
 
-def get_duration(yt_id):
-    """
-    Returns the duration for a given
-    """
+def get_video_info(yt_id):
+    """ Returns the video duration, url for the provided youtube id. """
     r = requests.get(YT_GDATA.format(yt_id))
-    return r.json()['entry']['media$group']['yt$duration']['seconds']
+    duration = r.json()['entry']['media$group']['yt$duration']['seconds']
+    url = r.json()['entry']['link'][0]['href']
+    return duration, url
 
 def time_to_seconds(time):
     """
-    Takes a string in the form of "%M:%S" and returns the time in seconds
+    Takes a string in the form of "%M:%S" and returns the total seconds.
+    Can also handle cases where only ":%S" is provided.
     """
     m,s = [int(x) if x != '' else 0 for x in time.split(':')]
     return 60*m + s
 
-def build_response(time, new_url):
+def build_reply(time, new_url):
     """
-    Constructs and returns the response
+    Constructs and returns the full reply that will be posted to Reddit
     """
-    response = u'[{0}]({1})'.format(time, new_url) + FOOTER
-    return response
+    return u'[{0}]({1})'.format(time, new_url) + FOOTER
+
+def is_unique(comment):
+    """
+    Checks to see if the bot has already posted in the current comment's subtree
+
+    The main purpose of this is so that no one messes with the bot.
+    Returns True if the bot has not posted yet, False otherwise.
+    """
+    if not comment.is_root:
+        current = REDDIT.get_info(thing_id=comment.parent_id)
+        while current.is_root == False:
+            if current.author.name == BOT_NAME:
+                return False
+            current = REDDIT.get_info(thing_id=current.parent_id)
+    return True
 
 def handle_comment(comment):
     """
     Parses through the comment object's text to see if it contains the
-    regex we are looking for. If it does, then handles all of the work for it,
-    else does nothing at all.
+    regex we are looking for. If it does, then handles all of the work for it
+    and then replies.
+
+    Returns the newly created comment if it replied, else returns None.
 
     Eek this function is kind of ugly
     """
-    if (not TIME_REGEX.match(comment.body)):
+    if comment.author.name == BOT_NAME:
         return 
+
+    if not TIME_REGEX.match(comment.body):
+        return 
+    time = TIME_REGEX.match(comment.body).group(1)
 
     log_comment(comment)
 
-    # This is not the best way to do it, but should for for now
-    if (LINK_REGEX.search(comment.body)):
+    if JIFFY_REGEX.search(comment.body):
+        print "Damn you jiffy!"
+        LOGGER.info("Jiffy comment...skipping!")
         return
-   
-    url = HTMLParser.HTMLParser().unescape(comment.submission.url)
-    youtube_id = get_youtube_id(url)
-    time = TIME_REGEX.match(comment.body).group(1)
 
+    # This is not the best way to do it, but should do for now
+    if LINK_REGEX.search(comment.body):
+        print "How dare you take my job!"
+        LOGGER.info("Already contains a link...skipping!")
+        return
+
+    if not is_unique(comment):
+        print "Stop messing with lazybot!"
+        LOGGER.info("Bot already posted in this subtree...skipping!")
+        return
+
+    orig_url = HTMLParser.HTMLParser().unescape(comment.submission.url)
+    youtube_id = get_youtube_id(orig_url)
     if not youtube_id:
+        LOGGER.info("Not a valid youtube link...skipping!")
         return 
 
     comment_secs = time_to_seconds(time)
-    video_secs = get_duration(youtube_id)
-
-    if int(comment_secs) >= int(video_secs):
+    duration, new_url = get_video_info(youtube_id)
+    LOGGER.info("Comment seconds: %s" % comment_secs)
+    LOGGER.info("Video seconds: %s" % duration)
+    if int(comment_secs) >= int(duration):
+        LOGGER.info("Comment seconds was greater...skipping!")
         return 
     elif comment_secs < 2:
         comment_secs = 0
     else:
         comment_secs -= 2
 
-    LOGGER.info("Comment seconds: %s" % comment_secs)
-    LOGGER.info("Video seconds: %s" % video_secs)
+    final_url = "{0}&t={1}".format(new_url, comment_secs)
+    LOGGER.info("Final url: %s" % final_url)
 
-    new_url = "{0}&t={1}".format(url, comment_secs)
-    LOGGER.info("New url: %s" % new_url)
-    response = build_response(time, new_url)
-    comment.reply(response)
-    print "Got one!"
+    reply = build_reply(time, final_url)
+    new_comment = comment.reply(reply)
+
+    print "Successfully replied to %s" % comment.permalink
+    LOGGER.info("New comment link: %s" % new_comment.permalink)
+    LOGGER.info("SUCCESSFULLY REPLIED!")
+
+    return new_comment
 
 def log_comment(comment):
+    """ Logging helper """
     submission = comment.submission
 
     LOGGER.info("")
+    LOGGER.info("")
     LOGGER.info("Matched ==> %s" % TIME_REGEX.match(comment.body).group(1))
+    LOGGER.info("Comment body: %s" % comment.body)
+    LOGGER.info("Comment author: %s" % comment.author.name)
+    LOGGER.info("Comment upvotes: %s" % comment.ups)
     LOGGER.info("Comment id: %s" % comment.id)
-    LOGGER.info("Time created: %s" % time.ctime(comment.created_utc))
-    LOGGER.info("Root: %s" % comment.is_root)
-    LOGGER.info("Likes: %s" % comment.likes)
+    LOGGER.info("Comment permalink: %s" % comment.permalink)
+    LOGGER.info("Comment created: %s" % time.ctime(comment.created_utc))
+    LOGGER.info("Comment is Root: %s" % comment.is_root)
     LOGGER.info("Submission id: %s" % submission.id)
     LOGGER.info("Submission title: %s" % submission.title)
     LOGGER.info("Submission created: %s" %  time.ctime(submission.created_utc))
-    LOGGER.info("Submission domain: %s" % submission.domain)
-    LOGGER.info("Submission url: %s" % submission.url)
-    LOGGER.info("Comment body: %s" % comment.body)
+    LOGGER.info("Original url: %s" % submission.url)
 
-def get_record():
+def get_handled():
+    """ Either returns a new deque or the one from disk if it exists """
     try:
-        with open(READ_RECORD, 'rb') as f:
+        with open(HANDLED_FILE, 'rb') as f:
             print "Unpickle!"
             return pickle.load(f)
     except IOError as e:
         LOGGER.info("Pickle file does not exist yet")
         return deque(maxlen=200)
 
+# Add in better argument handling
 def main():
     print "Started"
     LOGGER.info("lazyassbot has started!")
     login()
-    read = get_record() 
+    handled = get_handled() 
     print "Running"
     while True:
         try:
-            comments = get_comments()
+            comments = get_comments(subreddit_=sys.argv[1])
             for comment in comments:
-                if comment.id not in read and comment.author.name != BOT_NAME:
+                if comment.id not in handled:
+                    handled.append(comment.id)
                     handle_comment(comment)
-                    read.append(comment.id)
             time.sleep(30)
         except praw.errors.RateLimitExceeded as e:
             print "Sleeping for %s" % e.sleep_time
             LOGGER.info("Sleeping for %s" % e.sleep_time)
             time.sleep(e.sleep_time)
-        except (HTTPError, URLError, requests.ConnectionError) as e:
-            LOGGER.info("!!!!! HTTP/URL Error was raised: %s" % e.strerror)
-            print "Connection error!"
+        except (requests.ConnectionError, requests.HTTPError) as e:
+            LOGGER.info("")
+            LOGGER.info("!!!!!requests connection/Http Error was raised")
+            print "requests network error!"
+            time.sleep(180) # wait 3 minutes on error
+        except (HTTPError, URLError) as e:
+            LOGGER.info("")
+            LOGGER.info("!!!!!urllib2 HTTP/URL Error was raised")
+            print "urllib2 network error!"
             time.sleep(180) # wait 3 minutes on error
         except KeyboardInterrupt as e:
-            LOGGER.info("lazyassbot has been interrupted")
+            LOGGER.info("")
+            LOGGER.info("!!!!!lazyassbot has been interrupted")
             break
-    with open(READ_RECORD, 'wb') as f:
+    with open(HANDLED_FILE, 'wb') as f:
         print "Pickle!"
-        pickle.dump(read, f)
+        pickle.dump(handled, f)
     print "Stopped"
 
 if __name__ == '__main__':
